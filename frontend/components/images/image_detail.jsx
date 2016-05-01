7ccb7691eee174cf3e3307527a64b19d8aca2172
kasperkuo <kasper.kuo@gmail.com>
2016-05-01 14:09:06 -0700

    fixed navbar styling and show page

--- frontend/components/images/image_detail.jsx
+++ frontend/components/images/image_detail.jsx
@@ -51,11 +51,22 @@ var ImageDetail = React.createClass({
     HashHistory.push('/');
   },
 
+  goNextImage: function(e) {
+
+  },
+
+  goPreviousImage: function(e) {
+
+  },
+
   render: function() {
     var url;
     var id;
     var title;
     var editForm;
+    var deleteForm;
+    var description;
+    var imageUploader;
 
     if (this.state.image) {
       url = this.state.image.image_url;
@@ -66,26 +77,40 @@ var ImageDetail = React.createClass({
     console.log(SessionStore.isLogged());
 
     // if (SessionStore.isLogged()) {
-      editForm = <a onClick={this.editImage}>Edit</a>;
+    editForm = <a className="backExplore" onClick={this.editImage}>Edit</a>;
+    deleteForm = <a onClick={this.deleteImage} className="backExplore">Delete</a>;
+
     // }
+    imageUploader = <div className="imageOwner">Image uploader goes here.</div>;
+    description = <div className="imageDescription">Description goes here.</div>;
 
     return (
-      <div>
-        <div className="imageDetail">
+      <div className="imageContainer">
+        <div className="arrow" id="previous" onClick={this.goPreviousImage}>
+          <img src="http://image005.flaticon.com/1/svg/60/60573.svg"/>
+        </div>
 
-          <div className="imageUploadUser">{title}</div>
-          <br></br>
-          {editForm}
-          <br></br>
-          <img src={url} />
-          <br></br>
-          <a onClick={this.deleteImage} className="backExplore">Delete</a>
-          <br></br>
-          <br></br>
-          <a onClick={this.redirectHome} className="backExplore">Back to Explore</a>
+        <div className="imageInfo">
+          <h1 className="imageInfoHeader">{title}</h1>
+          {imageUploader}
+          {description}
+          <div className="userOptions">
+            {editForm}<span> / </span>{deleteForm}
+            <br></br>
+            <br></br>
+            <a onClick={this.redirectHome} className="backExplore">Back to Explore</a>
+          </div>
           <br></br>
         </div>
-      </div>
+
+        <div className="imageDetail">
+          <img src={url} />
+        </div>
+
+        <div className="arrow" id="next" onClick={this.goNextImage}>
+          <img src="http://image005.flaticon.com/1/svg/60/60615.svg" />
+        </div>
+    </div>
     );
   }
 
